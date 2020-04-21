d70beb2781a13d8c6716d03f5c3eeea4f1a44c8e
Swanka644 <Swati_Wankar@comcast.com>
2020-04-21 22:22:14 +0530

    second commit

--- src/test/java/Automation/facebook.java
+++ src/test/java/Automation/facebook.java
@@ -1,48 +1,37 @@
 package Automation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class facebook {
 
-	public static void main(String[] args) throws InterruptedException {
-		
-		
-		ChromeOptions optn=new ChromeOptions();
-		optn.addArguments("--incognito");
-		Thread.sleep(1000);
-		optn.addArguments("--start-maximized");
-		WebDriver driver=new ChromeDriver(optn);
-		/*optn.setPageLoadStrategy(PageLoadStrategy.NONE);*/
-		
-		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
-		/*driver.manage().window().maximize();*/
-		
-		//xpath
-		/*driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
-		driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
-		Actions action = new Actions(driver);
-		WebElement suboption=driver.findElement(By.xpath("//a[@id='menu__Performance']/child::b"));
-		WebElement confiOption=driver.findElement(By.xpath("//a[@id='menu__Performance']//following::ul/li/child::a[@id='menu_performance_Configure']"));
-		WebElement eps=driver.findElement(By.xpath("//a[text()='KPIs']"));
-		action.moveToElement(suboption).moveToElement(confiOption).moveToElement(eps).click().build().perform();*/
-	/*	
-		JavascriptExecutor js = (JavascriptExecutor)driver;		
-		
-		  WebElement elm =driver.findElement(By.xpath("//input[@id='txtUsername']"));		
-		  js.executeScript("arguments[0].click();alert(\"done\")", button);
-		  
-		  ((JavascriptExecutor) driver).executeScript("arguments[0].click();alert(\"Clicked on Register Button\"))",elm);
 
-                */
+		
+		private WebDriver driver; 
+		String URL = "http://google.com";
+
+		@BeforeClass
+		public void testSetUp() {
+			
+			driver = new ChromeDriver();
+		}
+		
+		@Test
+		public void verifyGooglePageTittle() {
+			driver.navigate().to(URL);
+			String getTitle = driver.getTitle();
+			Assert.assertEquals(getTitle, "Google");
+		}
+		
+		@AfterClass
+		public void tearDown() {
+			driver.quit();
+		}
 		
 	}
 
-}
+
