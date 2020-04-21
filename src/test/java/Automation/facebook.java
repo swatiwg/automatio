efe9d5ecefc95f4aa95b1710f4a5fb0e7f129860
Swanka644 <Swati_Wankar@comcast.com>
2020-04-21 22:44:22 +0530

    changes

--- src/test/java/Automation/facebook.java
+++ src/test/java/Automation/facebook.java
@@ -17,7 +17,7 @@ public class facebook {
 		@BeforeClass
 		public void testSetUp() {
 			
-			System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver","chromedriver");
 			driver = new ChromeDriver();
 		}
 		
